06f586d8f275b684b88ff9a8679b48f49ba16b55
mbanty834-ux <mbanty834@gmail.com>
2026-09-02 22:00:53 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,78 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+import pandas as pd
+import joblib
+
+app = FastAPI(
+    title="EV Battery Failure Prediction API",
+    version="1.0.0"
+)
+
+
+model = joblib.load("" \
+"battery_failure_model.pkl")
+
+
+
+
+class BatteryData(BaseModel):
+
+    battery_capacity_kwh: float = Field(..., example=60.5)
+    odometer_km: float = Field(..., example=45000)
+    vehicle_age_years: float = Field(..., example=4)
+    cycle_count: int = Field(..., example=800)
+
+    battery_health_percent: float = Field(..., example=92.5)
+    state_of_charge: float = Field(..., example=75.0)
+    depth_of_discharge: float = Field(..., example=65.0)
+    state_of_health: float = Field(..., example=90.0)
+
+    cell_voltage_std: float = Field(..., example=0.025)
+    cell_temperature_max: float = Field(..., example=42.5)
+    internal_resistance: float = Field(..., example=0.015)
+
+    capacity_loss_percent: float = Field(..., example=8.5)
+    charging_interruptions: int = Field(..., example=3)
+    overcharge_events: int = Field(..., example=1)
+
+@app.get("/")
+def home():
+    return {
+        "message": "EV Battery Failure Prediction API is running"
+    }
+
+
+@app.post("/predict")
+def predict(data: BatteryData):
+
+    try:
+       
+        input_data = data.model_dump()
+
+    
+        input_df = pd.DataFrame([input_data])
+
+      
+        prediction = model.predict(input_df)[0]
+
+        response = {
+            "prediction": str(prediction)
+        }
+
+      
+        if hasattr(model, "predict_proba"):
+            probability = model.predict_proba(input_df)[0]
+            response["probability"] = float(max(probability))
+
+        return response
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
+
+
+
+   
+   
